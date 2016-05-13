0191570802922481b6fb94ca973b0e179f80e54a
Mathias Fredriksson <mafredri@gmail.com>
2016-05-14 01:35:08 +0300

    Missing paren

--- src/extend.ts
+++ src/extend.ts
@@ -4,7 +4,7 @@ Screen.at = function at(point: Point) {
 	let screens = Screen.screens();
 
 	for (let s of screens) {
-		if (pointInsideFrame(point, s.frameInRectangle()) {
+		if (pointInsideFrame(point, s.frameInRectangle())) {
 			return s;
 		}
 	}
