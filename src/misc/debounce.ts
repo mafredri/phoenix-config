b8b314bff77e2d4adeea8b36513219cef30d0ff7
Mathias Fredriksson <mafredri@gmail.com>
2016-12-02 18:19:48 +0200

    Add missing debounce.ts

---
+++ src/misc/debounce.ts
@@ -0,0 +1,17 @@
+export default function debounce(func: () => void, wait: number, immediate?: boolean) {
+	let timeout: number;
+	return (...args: any[]) => {
+		const later = () => {
+			timeout = null;
+			if (!immediate) {
+				func.apply(this, args);
+			}
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) {
+			func.apply(this, args);
+		}
+	};
+};
