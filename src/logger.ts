a35fb92954fc279222b39fbc038817729098a089
Mathias Fredriksson <mafredri@gmail.com>
2016-12-26 21:25:23 +0200

    Use new Phoenix.log API

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,12 @@
 export default function log(...args: any[]): void {
 	args = args.map((arg) => stringify(arg));
-	Phoenix.log(args.join(' '));
+	Phoenix.log(...args);
 }
 
 function stringify(value: any) {
 	switch (typeof value) {
 		case 'object':
-			return JSON.stringify(value, null, 2);
+			return '\n' + JSON.stringify(value, null, 2);
 		case 'function':
 			return value.toString();
 		default:
