40a629ddae634a0b118036c967f22111fd88c74d
Mathias Fredriksson <mafredri@gmail.com>
2017-05-12 20:42:07 +0300

    env: Add getEnv for fetching environment vars

---
+++ src/misc/env.ts
@@ -0,0 +1,15 @@
+export function getEnv(name = '') {
+	return new Promise((resolve, reject) => {
+		if (!name) {
+			return reject('no variable name provided');
+		}
+
+		Task.run('/bin/sh', ['-c', `echo "$${name}"`], (t) => {
+			if (t.status === 0) {
+				return resolve(t.output);
+			} else {
+				return reject(`could not execute command to fetch '$${name}'`);
+			}
+		});
+	});
+}
