b8dad65c6ddcb197706f2545bdd259d82b2af11b
Mathias Fredriksson <mafredri@gmail.com>
2017-06-14 11:26:25 +0300

    Add notify() to logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,15 @@
-export default function log(...args: any[]): void {
-	args = args.map((arg) => stringify(arg));
+function log(...args: any[]): void {
+	args = args.map(arg => stringify(arg));
 	Phoenix.log(...args);
 }
+
+export default Object.assign(log, {
+	notify: (...args: any[]): void => {
+		log(...args);
+		const message = args.map(arg => stringify(arg)).join(' ');
+		Phoenix.notify(message);
+	},
+});
 
 function stringify(value: any) {
 	if (value instanceof Error) {
@@ -9,7 +17,7 @@ function stringify(value: any) {
 		if (value.stack) {
 			const s = value.stack.trim().split('\n');
 			s[0] += ` (:${value.line}:${value.column})`;
-			const indented = s.map((line) => '\t at ' + line).join('\n');
+			const indented = s.map(line => '\t at ' + line).join('\n');
 			stack = `\n${indented}`;
 		}
 		return `\n${value.name}: ${value.message}${stack}`;
