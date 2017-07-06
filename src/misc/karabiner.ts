465bbbb6826565184160c230fc5ce2a4ddded390
Mathias Fredriksson <mafredri@gmail.com>
2017-07-06 18:32:53 +0300

    karabiner: Use new string enums from TypeScript 2.4

--- src/misc/karabiner.ts
+++ src/misc/karabiner.ts
@@ -1,22 +1,11 @@
 export enum Profile {
-	Internal,
-	Mistel,
+	Internal = 'Internal Keyboard',
+	Mistel = 'Mistel Barocco',
 }
 
 const bin =
 	'/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli';
 
 export function selectProfile(p: Profile): void {
-	Task.run(bin, ['--select-profile', profileString(p)]);
-}
-
-function profileString(p: Profile): string {
-	switch (p) {
-		case Profile.Internal:
-			return 'Internal Keyboard';
-		case Profile.Mistel:
-			return 'Mistel Barocco';
-		default:
-			throw new Error('unknown profile');
-	}
+	Task.run(bin, ['--select-profile', p]);
 }
