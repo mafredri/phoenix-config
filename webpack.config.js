958fbb33cd5466b0d047c6f9943adeb88a6cd292
Mathias Fredriksson <mafredri@gmail.com>
2018-04-09 19:22:12 +0300

    Update modules, webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+const path = require('path');
+
 module.exports = {
+	mode: 'development',
 	entry: './src/phoenix.ts',
 	output: {
-		filename: 'out/phoenix.js',
+		path: path.resolve(__dirname, 'out'),
+		filename: 'phoenix.js',
 	},
 	resolve: {
 		extensions: ['.ts', '.js'],
 	},
 	module: {
-		loaders: [
+		rules: [
 			{ test: /\.ts$/, loader: 'ts-loader' },
 		],
 	},
