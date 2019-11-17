8a66f0021041ad90cb396c7504a458b25af5fd0c
Mathias Fredriksson <mafredri@gmail.com>
2019-11-17 12:30:51 +0200

    Frame cache improvements

--- src/window.ts
+++ src/window.ts
@@ -6,6 +6,10 @@ export {toggleMaximized, clearUnmaximized};
 interface FrameCache {
 	screen: Rectangle;
 	window: Rectangle;
+	maximized?: {
+		screen: Rectangle;
+		window: Rectangle;
+	};
 }
 
 const frameCache: Map<number, FrameCache> = new Map();
@@ -31,18 +35,74 @@ function unmaximizedFrame(win: Window): Rectangle {
 	return ratio(c.window);
 }
 
+function objEqual(a: {[key: string]: any}, b: {[key: string]: any}): boolean {
+	if (typeof a !== 'object') {
+		return a === b;
+	}
+	if (Object.keys(a).length !== Object.keys(b).length) {
+		return false;
+	}
+	for (const key of Object.keys(a)) {
+		if (typeof a[key] === 'object') {
+			if (!objEqual(a[key], b[key])) {
+				return false;
+			}
+		}
+		if (a[key] !== b[key]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function isMaximized(win: Window): boolean {
+	const cache = frameCache.get(win.hash());
+	if (!cache || !cache.maximized) {
+		return false;
+	}
+
+	log(win.frame(), cache.maximized.window);
+
+	return (
+		objEqual(win.screen().flippedVisibleFrame(), cache.maximized.screen) &&
+		objEqual(win.frame(), cache.maximized.window)
+	);
+}
+
 function toggleMaximized(win: Window) {
-	const id = win.hash();
-	if (frameCache.has(id)) {
+	if (isMaximized(win)) {
 		win.setFrame(unmaximizedFrame(win));
 		win.clearUnmaximized();
 		return;
 	}
-	frameCache.set(id, {
+	const previous = {
 		screen: win.screen().flippedVisibleFrame(),
 		window: win.frame(),
-	});
+	};
 	win.maximize();
+
+	const id = win.hash();
+	frameCache.set(id, {
+		...previous,
+		maximized: {
+			screen: win.screen().flippedVisibleFrame(),
+			window: win.frame(),
+		},
+	});
+}
+
+function setFrame(win: Window, frame: Rectangle): boolean {
+	const ok = win.setFrame(frame);
+	if (ok) {
+		// Invalidate cache.
+		// No maximized.
+	}
+	return ok;
+}
+
+function maximize(win: Window): boolean {
+	const ok = win.maximize();
+	return ok;
 }
 
 Window.prototype.clearUnmaximized = function _clearUnmaximized() {
