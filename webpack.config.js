bb76cc44361d85354a22d757a287b93c71fa243e
Mathias Fredriksson <mafredri@gmail.com>
2020-10-30 23:13:03 +0200

    Add development build with inline sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,45 @@
 const path = require('path');
 
-module.exports = {
-	mode: 'production',
-	entry: './src/phoenix.ts',
-	output: {
-		path: path.resolve(__dirname, 'out'),
-		filename: 'phoenix.js',
+module.exports = [
+	{
+		mode: 'production',
+		entry: './src/phoenix.ts',
+		output: {
+			path: path.resolve(__dirname, 'out'),
+			filename: 'phoenix.js',
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
 	},
-	resolve: {
-		extensions: ['.ts', '.js'],
+	{
+		mode: 'development',
+		entry: './src/phoenix.ts',
+		devtool: 'inline-source-map',
+		output: {
+			path: path.resolve(__dirname, 'out'),
+			filename: 'phoenix.debug.js',
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
 	},
-	module: {
-		rules: [{test: /\.ts$/, loader: 'ts-loader'}],
-	},
-};
+];
